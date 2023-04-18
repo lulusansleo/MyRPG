2d2339f497e94119a5aad5081dbba08b712f0211
TristanLtrg <tristan.leturgie@epitech.eu>
2023-04-18 16:50:51 +0200

    [ADD] added the call of the function for the player and the movemenets

--- source/main.c
+++ source/main.c
@@ -8,21 +8,25 @@
 #include "window.h"
 #include "map.h"
 #include "my.h"
+#include "player.h"
 
 int main(void)
 {
     sfRenderWindow *window = initalise_window();
     sfEvent *event = malloc(sizeof(sfEvent));
     layer_t *layers = initialise_layer();
+    player_t *player = init_player();
     while (sfRenderWindow_isOpen(window)) {
         while (sfRenderWindow_pollEvent(window, event)) {
             if (event->type == sfEvtClosed)
                 sfRenderWindow_close(window);
         }
+        get_move(player, layers[0]);
         sfRenderWindow_clear(window, sfBlack);
         draw_layer(layers[1].tiles, layers[1].sprite_sheet, window);
         draw_layer(layers[0].tiles, layers[0].sprite_sheet, window);
         draw_layer(layers[2].tiles, layers[2].sprite_sheet, window);
+        draw_player(player, layers[0]);
         sfRenderWindow_display(window);
     }
     sfRenderWindow_destroy(window);
