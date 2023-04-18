0200d84cf3695fd564c7cad58574ab60b37e7c59
lulusansleo <luan.bourbier@epitech.eu>
2023-04-18 13:04:19 +0200

    [add] full implementation of tile map

--- source/display/window.c
+++ source/display/window.c
@@ -9,7 +9,7 @@
 
 sfRenderWindow *initalise_window(void)
 {
-    sfVideoMode mode = {320, 320, 32};
+    sfVideoMode mode = {1000, 1000, 32};
     sfRenderWindow *window =
     sfRenderWindow_create(mode, "MyRPG", sfClose | sfResize, NULL);
     sfRenderWindow_setFramerateLimit(window, 60);
