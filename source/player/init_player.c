ace6d131b89168c247bc5c9c136c5ca5a5535923
TristanLtrg <tristan.leturgie@epitech.eu>
2023-04-21 18:10:56 +0200

    [ADD] modified the struct so i initialised the new values (dmg type direction)

--- source/player/init_player.c
+++ source/player/init_player.c
@@ -21,17 +21,29 @@ static sfIntRect get_plyr_rect(int type)
     }
 }
 
+int get_dmg(int type)
+{
+    if (type == HAND)
+        return 2;
+    if (type == SWORD)
+        return 10;
+    if (type == BOW)
+        return 5;
+    return 2;
+}
+
 entity_t *init_entity(char *path)
 {
     entity_t *entity = malloc(sizeof(entity_t));
     entity->pos = (sfVector2f){48, 48};
+    entity->type = BOW;
+    entity->hp = 100;
+    entity->dmg = get_dmg(entity->type);
     entity->speed = 2;
     entity->clock = sfClock_create();
     entity->mob_direction_clock = sfClock_create();
-    entity->type = BOW;
     entity->rect = get_plyr_rect(entity->type);
     entity->texture = sfTexture_createFromFile(TXT_PLYR, NULL);
-    sfTexture_createFromFile(TXT_PLYR, NULL);
     entity->sprite = sfSprite_create();
     sfSprite_setTexture
 (entity->sprite, entity->texture, sfTrue);
