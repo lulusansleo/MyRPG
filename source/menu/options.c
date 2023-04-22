09ae3d53c464d48f86feb7bb6fad3c98633290ce
Alexis MER <alexis.mer@epitech.eu>
2023-04-22 14:27:43 +0200

    [ADD] options menu display

---
+++ source/menu/options.c
@@ -0,0 +1,61 @@
+/*
+** EPITECH PROJECT, 2023
+** menu rpg
+** File description:
+** options
+*/
+
+#include "gamestate.h"
+#include "menu.h"
+
+void handle_close_event(sfRenderWindow *window, sfEvent event)
+{
+    if (event.type == sfEvtClosed)
+        sfRenderWindow_close(window);
+}
+
+int go_back_to_menu(sfRenderWindow *window, options_t *options)
+{
+    sfVector2i mouse_pos = sfMouse_getPositionRenderWindow(window);
+
+    if (sfFloatRect_contains(&(options->arrow_bounds), mouse_pos.x, mouse_pos.y)
+        && sfMouse_isButtonPressed(sfMouseLeft)) {
+        return 1;
+    }
+    return 0;
+}
+
+void display_options(sfRenderWindow *window, options_t *options)
+{
+    sfRenderWindow_drawSprite(window, options->background, NULL);
+    sfRenderWindow_drawText(window, options->options_text, NULL);
+    sfRenderWindow_drawText(window, options->fps_text, NULL);
+    sfRenderWindow_drawText(window, options->res_text, NULL);
+    sfRenderWindow_drawText(window, options->vol_text, NULL);
+    display_buttons(window, options->res_buttons, 3);
+    display_buttons(window, options->fps_buttons, 3);
+    display_buttons(window, options->vol_buttons, 2);
+    sfRenderWindow_drawSprite(window, options->back_arrow, NULL);
+}
+
+void run_options(menu_t *menu, gamestate_t *gamestate)
+{
+    sfRenderWindow *window = gamestate->window;
+    sfEvent *event = gamestate->event;
+    options_t *options = menu->options;
+
+    update_all_bounds(window, options);
+    while (sfRenderWindow_isOpen(window)) {
+        while (sfRenderWindow_pollEvent(window, event)) {
+            handle_close_event(window, *event);
+        }
+        if (go_back_to_menu(window, options) == 1)
+            break;
+        resize_window(window, options);
+        change_fps(window, options);
+        update_all_buttons(window, options);
+        sfRenderWindow_clear(window, sfBlack);
+        display_options(window, options);
+        sfRenderWindow_display(window);
+    }
+}
