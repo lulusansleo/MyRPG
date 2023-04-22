0d57aef5b2451f8302efc452ad415b256384214b
Alexis MER <alexis.mer@epitech.eu>
2023-04-22 14:20:04 +0200

    [ADD] basic menu screen display

---
+++ source/menu/menu.c
@@ -0,0 +1,50 @@
+/*
+** EPITECH PROJECT, 2023
+** menu
+** File description:
+** rpg
+*/
+
+#include "gamestate.h"
+#include "menu.h"
+#include <stdlib.h>
+
+void display_buttons(sfRenderWindow *window, menu_button_t *buttons, int nb)
+{
+    for (int i = 0; i < nb; ++i) {
+        sfRenderWindow_drawRectangleShape(window, buttons[i].button, NULL);
+        sfRenderWindow_drawText(window, buttons[i].text, NULL);
+    }
+}
+
+void display_menu(menu_button_t *buttons, sfRenderWindow *window, menu_t *menu)
+{
+    sfRenderWindow_clear(window, sfBlack);
+    sfRenderWindow_drawSprite(window, menu->background, NULL);
+    display_buttons(window, buttons, 4);
+    sfRenderWindow_display(window);
+}
+
+void menu(gamestate_t *gamestate, menu_button_t *buttons, menu_t *menu)
+{
+    sfEvent event;
+    sfRenderWindow *window = gamestate->window;    
+
+    sfRenderWindow_setView(window, sfRenderWindow_getDefaultView(window));
+    update_bounds(buttons, window, 4);
+    update_text_pos(buttons, 4);
+    while (sfRenderWindow_isOpen(window)) {
+        while (sfRenderWindow_pollEvent(window, &event)) {
+            handle_close_event(window, event);
+        }
+        update_states(buttons, window, 4);
+        update_colors(buttons, 4);
+        display_menu(buttons, window, menu);
+        if (buttons[0].state == CLICK)
+            break;
+        if (buttons[2].state == CLICK) {
+            run_options(menu, gamestate);
+            update_bounds(buttons, window, 4);
+        }
+    }
+}
