4eaea9769ac74d08b417036b9398b537f9008d24
TristanLtrg <tristan.leturgie@epitech.eu>
2023-04-22 18:09:28 +0200

    [ADD] attack on the player and phase of invicibilty for the player when getting hit by a mob

--- source/npc/attack_npc.c
+++ source/npc/attack_npc.c
@@ -8,6 +8,8 @@
 #include "map.h"
 #include "my.h"
 #include "player.h"
+#include "fight.h"
+#include "graphical.h"
 #include "npc.h"
 
 
@@ -19,13 +21,33 @@ float distance_npc(sfVector2f abs, sfVector2f ord)
     return sqrt(dx * dx + dy * dy);
 }
 
+static void is_attacking(entity_t *player, entity_t *mob)
+{
+    sfFloatRect *box_player = get_hitbox(player);
+    sfFloatRect *box_mob = get_hitbox(mob);
+    sfTime time;
+    if (player->invicible == 0 &&
+    sfFloatRect_intersects(box_player, box_mob, NULL) == sfTrue) {
+        player->invicibility = sfClock_create();
+        hit(mob, player);
+        player->invicible = 1;
+    }
+    if (player->invicible == 1) {
+        time = sfClock_getElapsedTime(player->invicibility);
+        if (sfTime_asSeconds(time) > 5.0 && player->invicible == 1) {
+            sfClock_destroy(player->invicibility);
+            player->invicible = 0;
+        }
+    }
+}
+
 void attack_player(entity_t *mob, entity_t *player, float deltaTime)
 {
+    is_attacking(player, mob);
     float angle = atan2(player->pos.y - mob->pos.y, player->pos.x - mob->pos.x);
 
     mob->pos.x += mob->speed * deltaTime * cos(angle);
     mob->pos.y += mob->speed * deltaTime * sin(angle);
-
     sfSprite_setPosition(mob->sprite, mob->pos);
 }
 
@@ -34,7 +56,7 @@ void npc_move(entity_t *player, npc_t *head)
     if (head == NULL)
         return;
     int direction;
-    npc_t *tmp = head; 
+    npc_t *tmp = head;
     while (tmp != NULL) {
         float deltaTime = sfTime_asSeconds(sfClock_getElapsedTime(tmp->mob->clock));
         float mob_distance = distance_npc(player->pos, tmp->mob->pos);
