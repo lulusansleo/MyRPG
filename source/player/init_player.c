31ade5114f8583b96f94f9993c8e71893b1a17c1
Zerrouk <adam.zerrouk@epitech.eu>
2023-04-23 13:16:43 +0200

    [ADD]-adding file for merging

--- source/player/init_player.c
+++ source/player/init_player.c
@@ -46,9 +46,8 @@ void init_values(entity_t *entity)
 entity_t *init_entity(void)
 {
     entity_t *entity = malloc(sizeof(entity_t));
-    entity->pos = (sfVector2f){100, 48};
+    entity->pos = (sfVector2f){500, 500};
     entity->move = (sfVector2f){0, 0};
-    entity->pos = (sfVector2f){48, 48};
     entity->clock = sfClock_create();
     entity->weapon = NULL;
     init_values(entity);
